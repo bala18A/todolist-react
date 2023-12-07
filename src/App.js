2cc30784efe3009effe2ca70c635614250dcf06c
bala18A <chandrabala1820@gmail.com>
2023-12-07 12:20:14 +0530

    f

--- src/App.js
+++ src/App.js
@@ -1,80 +1,4 @@
 
-import React, {  useState } from 'react'
-// import "./todolistinput box.css"
-import Header from './Header'
-
-import Content from './Content'
-import Foot from './Foot'
-import Additems from './Additems'
-
-
-  const App = () => {
-    const [data, setdata] = useState(JSON.parse(localStorage.getItem("todolist")))
-
-  const [inputvalue, setinputvalue] = useState('')
-  const [searchvalue, setsearchvalue] = useState('')
-
-
-  const handledelete = (id) => {
-    const listitems = data.filter(items =>
-      items.id !== id)
-    setdata(listitems)
-    localStorage.setItem("todolist",JSON.stringify(listitems))
-  }
-  const handlechange = (id) => {
-    const listitems = data.map(data =>
-      data.id === id ? { ...data, checked: !data.checked } : data)
-
-    setdata(listitems)
-    localStorage.setItem("todolist",JSON.stringify(listitems))
-
-  }
-  const handlesubmit = (e) => {
-    e.preventDefault()
-    if(!inputvalue.trim())
-    {
-      return 
-    }
-
-    additems(inputvalue)
-    // ===
-    setinputvalue('')
-  }
-  const additems = async(item) => {
-    const id = data.length ? data[data.length - 1].id + 1 : 1
-    const result = { id, checked: false, skill: item }
-    const listitems = [...data, result]
-    setdata(listitems)
-    localStorage.setItem("todolist",JSON.stringify(listitems))
-
-
-  }
-  return (
-   <div>
-
-     <Header header={'To Do List'} />
-      <div className='hole-content'>
-      <Additems
-        inputvalue={inputvalue}
-        setinputvalue={setinputvalue}
-        handlesubmit={handlesubmit}
-        searchvalue={searchvalue}
-        setsearchvalue={setsearchvalue} />
-      <Content
-        data={data.filter(items => items.skill.includes(searchvalue.toLowerCase()))} /* search venuna cmt remove*/
-        // data={data}//search venana ithu use pannu
-        setdata={setdata}
-        handledelete={handledelete}
-        handlechange={handlechange} />
-        </div>
-      <Foot footer={'Thank You'} />
-   </div>
-  );
-}
-
-export default App;
-// ==============================without local storage================
-
 // import React, {  useState } from 'react'
 // // import "./todolistinput box.css"
 // import Header from './Header'
@@ -85,11 +9,8 @@ export default App;
 
 
 //   const App = () => {
-//     const [data, setdata] = useState([
-//       {id:1,checked:true,skill:'play'},
-//       {id:2,checked:true,skill:'play'},
-//       {id:3,checked:true,skill:'play'},
-//     ])
+//     const [data, setdata] = useState(JSON.parse(localStorage.getItem("todolist")))
+
 //   const [inputvalue, setinputvalue] = useState('')
 //   const [searchvalue, setsearchvalue] = useState('')
 
@@ -98,14 +19,14 @@ export default App;
 //     const listitems = data.filter(items =>
 //       items.id !== id)
 //     setdata(listitems)
-//     // localStorage.setItem("todolist",JSON.stringify(listitems))
+//     localStorage.setItem("todolist",JSON.stringify(listitems))
 //   }
 //   const handlechange = (id) => {
 //     const listitems = data.map(data =>
 //       data.id === id ? { ...data, checked: !data.checked } : data)
 
 //     setdata(listitems)
-//     // localStorage.setItem("todolist",JSON.stringify(listitems))
+//     localStorage.setItem("todolist",JSON.stringify(listitems))
 
 //   }
 //   const handlesubmit = (e) => {
@@ -124,7 +45,7 @@ export default App;
 //     const result = { id, checked: false, skill: item }
 //     const listitems = [...data, result]
 //     setdata(listitems)
-//     // localStorage.setItem("todolist",JSON.stringify(listitems))
+//     localStorage.setItem("todolist",JSON.stringify(listitems))
 
 
 //   }
@@ -152,6 +73,85 @@ export default App;
 // }
 
 // export default App;
+// ==============================without local storage================
+
+import React, {  useState } from 'react'
+// import "./todolistinput box.css"
+import Header from './Header'
+
+import Content from './Content'
+import Foot from './Foot'
+import Additems from './Additems'
+
+
+  const App = () => {
+    const [data, setdata] = useState([
+      {id:1,checked:true,skill:'play'},
+      {id:2,checked:true,skill:'play'},
+      {id:3,checked:true,skill:'play'},
+    ])
+  const [inputvalue, setinputvalue] = useState('')
+  const [searchvalue, setsearchvalue] = useState('')
+
+
+  const handledelete = (id) => {
+    const listitems = data.filter(items =>
+      items.id !== id)
+    setdata(listitems)
+    // localStorage.setItem("todolist",JSON.stringify(listitems))
+  }
+  const handlechange = (id) => {
+    const listitems = data.map(data =>
+      data.id === id ? { ...data, checked: !data.checked } : data)
+
+    setdata(listitems)
+    // localStorage.setItem("todolist",JSON.stringify(listitems))
+
+  }
+  const handlesubmit = (e) => {
+    e.preventDefault()
+    if(!inputvalue.trim())
+    {
+      return 
+    }
+
+    additems(inputvalue)
+    // ===
+    setinputvalue('')
+  }
+  const additems = async(item) => {
+    const id = data.length ? data[data.length - 1].id + 1 : 1
+    const result = { id, checked: false, skill: item }
+    const listitems = [...data, result]
+    setdata(listitems)
+    // localStorage.setItem("todolist",JSON.stringify(listitems))
+
+
+  }
+  return (
+   <div>
+
+     <Header header={'To Do List'} />
+      <div className='hole-content'>
+      <Additems
+        inputvalue={inputvalue}
+        setinputvalue={setinputvalue}
+        handlesubmit={handlesubmit}
+        searchvalue={searchvalue}
+        setsearchvalue={setsearchvalue} />
+      <Content
+        data={data.filter(items => items.skill.includes(searchvalue.toLowerCase()))} /* search venuna cmt remove*/
+        // data={data}//search venana ithu use pannu
+        setdata={setdata}
+        handledelete={handledelete}
+        handlechange={handlechange} />
+        </div>
+      <Foot footer={'Thank You'} />
+   </div>
+  );
+}
+
+export default App;
 // =======================================to do list without local storage insert edit button
 
 // import React, { useState } from 'react'
